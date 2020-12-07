230d2dfec8c8b21b56df16520af9a82e52fb5a2a
Yana Malanii <miramis134@gmail.com>
2020-12-07 13:30:31 +0200

    code style/prettier

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-
 import * as React from "react";
 
 function App() {
-    const [count,setCount] = React.useState(0);
-    const increment =() =>{
-        setCount(count + 1)
-    };
-    const decrement =() =>{
-        setCount(count - 1)
-    };
+  const [count, setCount] = React.useState(0);
+  const increment = () => {
+    setCount(count + 1);
+  };
+  const decrement = () => {
+    setCount(count - 1);
+  };
   return (
-      <div>
-          <button onClick={increment}>Increment</button>
-          <p>{count}</p>
-          <button onClick={decrement}>Decrement</button>
-      </div>
-
+    <div>
+      <button onClick={increment}>Increment</button>
+      <p>{count}</p>
+      <button onClick={decrement}>Decrement</button>
+    </div>
   );
 }
 
